684a5284e8d8bd99ab77825f2f2491e8c24e7f2e
farhanalfauzi <farhanalfauzii@gmail.com>
2021-03-03 22:24:44 +0700

    penyelesaian tugas 1

---
+++ praktikum/tugas1/Latihan1a.php
@@ -0,0 +1,10 @@
+<?php
+// Farhan Alfauzi
+// 203040085
+// Shift Kamis 08.00 - 09.00
+
+for ($i = 1; $i < 4; $i++) {
+    for ($z = 1; $z < 4; $z++) {
+        echo "Ini perulangan ke ($i,$z)<br>";
+    }
+}
