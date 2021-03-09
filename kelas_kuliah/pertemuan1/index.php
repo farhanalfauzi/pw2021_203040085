9043e0e3d35dec10e002036bf01bea581c8e1ae1
farhanalfauzi <farhanalfauzii@gmail.com>
2021-03-09 07:08:41 +0700

    menyelesaikan pertemuan 1

--- kelas_kuliah/pertemuan1/index.php
+++ kelas_kuliah/pertemuan1/index.php
@@ -1,3 +1,13 @@
+<?php 
+/*
+Farhan Alfauzi
+2030400085
+https://github.com/farhanalfauzi/pw2021_203040085
+pertemuan 1
+PERSIAPAN LINGKUNGAN PENGEMBANGAN
+*/
+?>
+
 <?php
     echo "Hello World!";
 ?>
