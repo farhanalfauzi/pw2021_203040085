5464993fd3895e72c258b3af7246a2641c9a563d
farhanalfauzi <farhanalfauzii@gmail.com>
2021-03-17 19:32:17 +0700

    menyelesaikan tugas 3

--- praktikum/tugas3/latihan3b.php
+++ praktikum/tugas3/latihan3b.php
@@ -22,7 +22,7 @@ $pemain_bola = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan3b_2030400085</title>
+    <title>latihan3b_2030400085</title>
 </head>
 <body>
     <h3>Daftar Pemain Bola Terkenal</h3>
