d591637f7f3ec345e0b8a7743755b20745856242
farhanalfauzi <farhanalfauzii@gmail.com>
2021-07-25 12:25:44 +0700

    menyelesaikan pertemuan 12

--- kelas_kuliah/pertemuan12/functions.php
+++ kelas_kuliah/pertemuan12/functions.php
@@ -67,6 +67,22 @@ function ubah($data) {
   return mysqli_affected_rows($conn);
 }
 
+function cari($keyword) {
+  $conn = koneksi();
+
+  $query = "SELECT * FROM mahasiswa
+            WHERE nama LIKE '%$keyword%' OR nrp LIKE '%$keyword%'";
+
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
 function login($data) {
   $conn = koneksi();
   
--- kelas_kuliah/pertemuan12/index.php
+++ kelas_kuliah/pertemuan12/index.php
@@ -8,6 +8,12 @@ if(!isset($_SESSION['login'])) {
 
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa");
+
+// ketika tombol cari diklik
+if(isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,6 +35,12 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
   <a href="tambah.php">Tambah Data Mahasiswa</a>
   <br><br>
 
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="masukkan keyword pencarian.." autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari!</button>
+  </form>
+  <br>
+
   <table border="1" cellpadding="10" cellspacing="0">
     <tr>
       <th>#</th>
@@ -36,6 +48,12 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
       <th>Nama</th>
       <th>Aksi</th>
     </tr>
+
+    <?php if(empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4"><p style="color: red; font-style: italic;">data mahasiswa tidak ditemukan!</p></td>
+    </tr>
+    <?php endif; ?>
 
     <?php $i =1;
     foreach($mahasiswa as $m) : ?>
